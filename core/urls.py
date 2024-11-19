54a0ffbf3ccc712539991ac130fb21e326482c67
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-11-19 18:45:47 -0500

    thought: end this suffering please

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,14 @@ from .views import (RegisterView, LoginView,
                    PremioListView, PremioDetailView,
                    TicketListView, TicketDetailView,
                    SorteoView,
-                   PremioSorteoDetailView, PremioSorteoListCreateView
+                   PremioSorteoDetailView, PremioSorteoListCreateView,
+                   CustomTokenObtainPairView, CustomTokenRefreshView
                    )
 urlpatterns = [
-        path('register/', RegisterView.as_view(), name='register'),
+    
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('users/', UserListView.as_view(), name='user-list'),  
     path('users/<int:pk>/', UserRetrieveView.as_view(), name='user-detail'), 
